b41d68bf29af7ec4ecef66a5e84d2a64a0242eaf
Steve Ivy <steve.ivy@wellsfargo.com>
2016-12-13 08:06:00 -0700

    Support macro args, support for template arg

--- _plugins/embed_post.rb
+++ _plugins/embed_post.rb
@@ -4,9 +4,10 @@
 require 'liquid'
 
 def embed_posts(payload)
-  re = /\[% embed_post url=(.*) %\]/
+  re = /\[%[\s]*embed_post (.*?)[\s]*%\]/
   content = payload.page.content
   site = payload.site
+
   includes_dir = site['includes_dir']
 
   embed_html = File.read("#{includes_dir}/embed.html")
@@ -17,14 +18,26 @@ def embed_posts(payload)
 
   out_content = "#{content}"
   content.scan re do |m|
-    url = m[0]
+    arg_s = m[0]
+    arg_h = Hash[ arg_s.scan(/([\w]+)=([\S]+)/).collect {
+      |x| [x[0], x[1]]
+    }]
+    url = arg_h['url']
     puts "Found url: #{url}"
+
+    if not arg_h['template'].nil?
+      template = arg_h['template']
+      puts "Found template: #{template}"
+      embed_html = File.read("#{includes_dir}/#{template}")
+      embed_t = Liquid::Template.parse(embed_html)
+    end
 
     page = page_hash[url]
     raise ArgumentError.new "No page with url #{url}." if page.nil?
 
     embed_rendered = embed_t.render('embed' => page)
-    replace = "[% embed_post url=#{url} %\]"
+
+    replace = "[% embed_post #{arg_s} %\]"
     out_content.gsub! replace, embed_rendered
   end
 
